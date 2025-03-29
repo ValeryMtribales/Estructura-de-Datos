58f6c5c7305f0a6aa150896eb0258cfb963b2c74
ValeryMtribales <valery.tribales@campusucc.edu.co>
2025-03-29 01:50:44 +0000

    main

--- practicacolas/practicaColas/model/ticket.py
+++ practicacolas/practicaColas/model/ticket.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional 
+from typing import Optional
 
 class Ticket(BaseModel):
     name: str # name of the person
@@ -8,4 +8,4 @@ class Ticket(BaseModel):
     case_description: str # description of the case
     age: int # age of the person
     priority_attention: bool # priority attention? True or False
-    priority: Optional[int] =  None #priority calculated by the system
+    priority: Optional[int] = None  # Prioridad calculada por el sistema; no enviada por el cliente 
