8dfab2cabd071dc33bf2287b4c48e8ce00226d62
ValeryMtribales <valery.tribales@campusucc.edu.co>
2025-04-26 17:50:05 +0000

    Directorio

--- Ejercicio excel/EjercicioP.py
+++ Ejercicio excel/EjercicioP.py
@@ -1,14 +1,45 @@
 import pandas as pd
 
+class Directorio:
+    def __init__(self, nombre:str, nit:int, sede:str, municipio:str):
+        self.nombre = nombre 
+        self.nit = nit 
+        self.sede = sede 
+        self.municipio = municipio 
+
+    def __str__(self):
+      return f"NOMBRE: {self.nombre}, NIT: {self.nit}, SEDE: {self.sede}, MUNICIPIO: {self.municipio}" 
+
+class Nodo:
+    def __init__(self, directorio:Directorio):
+        self.directorio = directorio 
+        self.izquierda = None
+        self.derecha = None
+       
 directorio = pd.read_csv('Ejercicio excel/Directorio.csv')
-print(directorio.head()) 
-print(directorio.dtypes) 
-directorio['Número NIT'] = directorio ['Número NIT'].str.replace(',','')
+#print(directorio.head()) 
+directorio.rename(columns={
+    'Razón Social Organización': 'nombre',
+    'Número NIT': 'nit',
+    'Nombre Sede': 'sede',
+    'Nombre Municipio': 'municipio',
+}, inplace=True)
+
+directorio['nit'] = directorio ['nit'].str.replace(',','')
 print(directorio.head()) 
 print(directorio.dtypes)
-directorio['Número NIT'] = directorio ['Número NIT'].astype(int)
+directorio['nit'] = directorio ['nit'].astype(int)
 print(directorio.head()) 
 print(directorio.dtypes)
 
 print(directorio.columns)
-print(directorio['Número NIT'])
+print(directorio['nit'])
+
+for index, row in directorio.iterrows():
+    directorio = Directorio(
+        nombre=row['nombre'],
+        nit=row['nit'],
+        sede=row['sede'],
+        municipio=row['municipio']
+    )
+    print(directorio) 
