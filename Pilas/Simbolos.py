ebfd7ad77e836dfcc7b4059ec29c0e44b311dba9
ValeryMtribales <valery.tribales@campusucc.edu.co>
2025-05-24 00:10:53 +0000

    Simbolos

--- Pilas/Simbolos.py
+++ Pilas/Simbolos.py
@@ -1,8 +1,33 @@
+class NodoPila:
+    def __init__(self, valor, siguiente=None):
+        self.valor = valor
+        self.siguiente = siguiente
+
+class PilaNodos:
+    def __init__(self):
+        self.cima = None
+
+    def push(self, valor):
+        nuevo = NodoPila(valor)
+        nuevo.siguiente = self.cima
+        self.cima = nuevo
+
+    def pop(self):
+        if self.cima is None:
+            return None
+        valor = self.cima.valor
+        self.cima = self.cima.siguiente
+        return valor
+
+    def vacia(self):
+        return self.cima is None
+
+
 def verificar_balanceo(expresion):
     pila = PilaNodos()
-    # No usamos diccionario, solo ifs para cada caso
+
     for simbolo in expresion:
-        if simbolo == '(' or simbolo == '{' or simbolo == '[':
+        if simbolo in "({[":
             pila.push(simbolo)
         elif simbolo == ')':
             tope = pila.pop()
@@ -16,15 +41,14 @@ def verificar_balanceo(expresion):
             tope = pila.pop()
             if tope != '[':
                 return False
-    if pila.vacia():
-        return True
-    else:
-        return False
+
+    return pila.vacia()
 
 
+# Pruebas
 expresiones = ["{[()()]}", "[(])", "{(a+b) * [c/d]}", "[{()}]", "{(a+b]}"]
 for expr in expresiones:
     if verificar_balanceo(expr):
         print(expr + ": Balanceado")
     else:
-        print(expr + ": No balanceado")
+        print(expr + ": No balanceado")
